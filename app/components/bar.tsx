f857c963871fcb419e796026413d3a7341a7cd58
Mat-hew-24 <amonline2005@gmail.com>
2025-07-11 17:48:49 +0530

    CHECKPOINT 1

--- app/components/bar.tsx
+++ app/components/bar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { MutableRefObject } from "react";
+import Image from "next/image";
 
 type Barprop = {
   changeTime: (x: number) => void;
@@ -63,30 +64,38 @@ export default function Bar({
   return (
     <>
       <div className="bg-black flex justify-start gap-4 w-[500px] mt-4 mb-12 rounded-3xl p-2 px-6 shadow-[1px_1px_2px_rgba(0,0,0,1)]">
-        <button
-          onClick={() => {
-            handleclick(15);
-          }}
-          className="barbtn"
-        >
-          15
-        </button>
-        <button
-          onClick={() => {
-            handleclick(30);
-          }}
-          className="barbtn"
-        >
-          30
-        </button>
-        <button
-          onClick={() => {
-            handleclick(60);
-          }}
-          className="barbtn"
-        >
-          60
-        </button>
+        <div className="mr-50 ml-2 flex gap-4">
+            <button
+            onClick={() => {
+              handleclick(15);
+            }}
+            className="barbtn"
+          >
+            15
+          </button>
+          <button
+            onClick={() => {
+              handleclick(30);
+            }}
+            className="barbtn"
+          >
+            30
+          </button>
+          <button
+            onClick={() => {
+              handleclick(60);
+            }}
+            className="barbtn"
+          >
+            60
+          </button>
+        </div>
+
+        <div>
+            <button className="barbtn p-2 filter invert hover:invert-0">
+              <Image src="/reload.png" alt="no" width={20} height={20} />
+            </button>
+        </div>
       </div>
 
       {loader && <div className="w-full h-full pt-30">{Spinner}</div>}
