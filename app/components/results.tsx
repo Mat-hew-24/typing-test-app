163635383939b2110575639a85a820f19622c8cd
Ans Christy <jovanacarmel@gmail.com>
2025-07-28 15:15:52 +0530

    Modified UI

--- app/components/results.tsx
+++ app/components/results.tsx
@@ -2,7 +2,7 @@ type Resultsprop = { accuracy: number; wpm: number };
 
 export default function Results({ accuracy, wpm }: Resultsprop) {
   return (
-    <div className=" bg-red-400 p-6 mt-3 rounded-lg shadow-lg mr-10">
+    <div className="bg-purple-900 p-6 mt-3 rounded-lg shadow-lg mr-10">
       <div className="text-[var(--text-color)] text-2xl m-auto mb-4">
         wpm
         <div className="text-5xl">{wpm.toFixed(0)}</div>
--- app/components/statbar.tsx
+++ app/components/statbar.tsx
@@ -2,7 +2,7 @@ type Statbarprop = { raw: number; accuracy: number; mode: number; consistency:nu
 
 export default function Statbar({ raw, accuracy, mode,consistency }: Statbarprop) {
   return (
-    <div className="text-[var(--text-color)] flex w-270 gap-[240px] pl-10 bg-amber-400 rounded-lg pt-4 pb-4 shadow-lg mt-4">
+    <div className="text-[var(--text-color)] text-xl flex w-270 text-center justify-center gap-[240px] mx-auto bg-purple-900 rounded-lg pt-4 pb-4 shadow-lg mt-4">
       <div>
         raw
         <div>{raw.toFixed(0)}</div>
