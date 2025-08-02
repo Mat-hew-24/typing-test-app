459bca1d7133ec2457d64bd6b5dc8caf06abb99b
Ans Christy <jovanacarmel@gmail.com>
2025-08-03 00:10:02 +0530

    added ui enhancements

--- app/components/results.tsx
+++ app/components/results.tsx
@@ -3,13 +3,13 @@ type Resultsprop = { accuracy: number; wpm: number };
 export default function Results({ accuracy, wpm }: Resultsprop) {
   return (
     <div className="bg-[var(--box-color)] p-6 mt-3 rounded-lg shadow-lg mr-10">
-      <div className="text-[var(--text-color)] text-2xl m-auto mb-4">
+      <div className="text-[var(--timer-color)] text-4xl m-auto mb-4">
         wpm
-        <div className="text-5xl">{wpm.toFixed(0)}</div>
+        <div className="text-5xl text-[var(--hover-color)]">{wpm.toFixed(0)}</div>
       </div>
-      <div className="text-[var(--text-color)] text-2xl m-auto">
+      <div className="text-[var(--timer-color)] text-4xl m-auto">
         acc
-        <div className="text-5xl">{accuracy.toFixed(0)}%</div>
+        <div className="text-5xl text-[var(--hover-color)]">{accuracy.toFixed(0)}%</div>
       </div>
     </div>
   );
--- app/components/statbar.tsx
+++ app/components/statbar.tsx
@@ -8,22 +8,22 @@ export default function Statbar({ raw, accuracy, mode,consistency }: Statbarprop
   }
   
   return (
-    <div className="text-[var(--text-color)] text-xl flex w-270 text-center justify-center gap-[240px] mx-auto bg-[var(--box-color)] rounded-lg pt-4 pb-4 shadow-lg mt-4">
+    <div className="text-[var(--timer-color)] text-2xl flex w-300 text-center justify-center gap-[240px] mx-auto bg-[var(--box-color)] rounded-lg pt-4 pb-4 shadow-lg mt-4">
       <div>
         raw
-        <div>{raw.toFixed(0)}</div>
+        <div className = "text-[var(--hover-color)]">{raw.toFixed(0)}</div>
       </div>
       <div>
         accuracy
-        <div>{accuracy.toFixed(0)}%</div>
+        <div className = "text-[var(--hover-color)]">{accuracy.toFixed(0)}%</div>
       </div>
       <div>
         mode
-        <div>{mode}s</div>
+        <div className = "text-[var(--hover-color)]">{mode}s</div>
       </div>
       <div>
         consistency
-        <div>{
+        <div className = "text-[var(--hover-color)]">{
         consistencyVal.toFixed(0)
         }%</div>
       </div>
