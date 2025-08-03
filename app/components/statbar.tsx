530364ac9b59f13d87afebeb5a768c8b9cf1d280
Mat-hew-24 <amonline2005@gmail.com>
2025-08-03 18:48:13 +0530

    some changes in ui

--- app/components/statbar.tsx
+++ app/components/statbar.tsx
@@ -8,22 +8,22 @@ export default function Statbar({ raw, accuracy, mode,consistency }: Statbarprop
   }
   
   return (
-    <div className="text-[var(--timer-color)] text-2xl flex w-300 text-center justify-center gap-[240px] mx-auto bg-[var(--box-color)] rounded-lg pt-4 pb-4 shadow-lg mt-4">
+    <div className="text-blue-600 text-2xl flex w-300 text-center justify-center gap-[240px] mx-auto bg-gradient-to-r from-blue-50 to-indigo-100 border border-blue-200 rounded-lg pt-4 pb-4 shadow-lg mt-4">
       <div>
         raw
-        <div className = "text-[var(--result-color)]">{raw.toFixed(0)}</div>
+        <div className = "text-indigo-700 font-semibold">{raw.toFixed(0)}</div>
       </div>
       <div>
         accuracy
-        <div className = "text-[var(--result-color)]">{accuracy.toFixed(0)}%</div>
+        <div className = "text-indigo-700 font-semibold">{accuracy.toFixed(0)}%</div>
       </div>
       <div>
         mode
-        <div className = "text-[var(--result-color)]">{mode}s</div>
+        <div className = "text-indigo-700 font-semibold">{mode}s</div>
       </div>
       <div>
         consistency
-        <div className = "text-[var(--result-color)]">{
+        <div className = "text-indigo-700 font-semibold">{
         consistencyVal.toFixed(0)
         }%</div>
       </div>
