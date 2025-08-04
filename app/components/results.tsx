90424d16a9e4643a9786683d238bff5733429c05
Mat-hew-24 <amonline2005@gmail.com>
2025-08-04 13:20:48 +0530

    FIXED TYPING BOX UI

--- app/components/results.tsx
+++ app/components/results.tsx
@@ -2,14 +2,14 @@ type Resultsprop = { accuracy: number; wpm: number };
 
 export default function Results({ accuracy, wpm }: Resultsprop) {
   return (
-    <div className="bg-gradient-to-br from-blue-50 to-indigo-100 border border-blue-200 p-6 mt-3 rounded-lg shadow-lg mr-10">
-      <div className="text-blue-600 text-4xl m-auto mb-4">
+    <div className="bg-[var(--resStatBoxColor)] p-6 mt-3 rounded-lg shadow-lg mr-10">
+      <div className="text-[var(--resStatTextColor)] text-4xl m-auto mb-4">
         wpm
-        <div className="text-5xl text-indigo-700 font-bold">{wpm.toFixed(0)}</div>
+        <div className="text-5xl text-[var(--resStatTextColor)] font-bold">{wpm.toFixed(0)}</div>
       </div>
-      <div className="text-blue-600 text-4xl m-auto">
+      <div className="text-[var(--resStatTextColor)] text-4xl m-auto">
         acc
-        <div className="text-5xl text-indigo-700 font-bold">{accuracy.toFixed(0)}%</div>
+        <div className="text-5xl text-[var(--resStatTextColor)] font-bold">{accuracy.toFixed(0)}%</div>
       </div>
     </div>
   );
--- app/components/statbar.tsx
+++ app/components/statbar.tsx
@@ -8,22 +8,22 @@ export default function Statbar({ raw, accuracy, mode,consistency }: Statbarprop
   }
   
   return (
-    <div className="text-blue-600 text-2xl flex w-300 text-center justify-center gap-[240px] mx-auto bg-gradient-to-r from-blue-50 to-indigo-100 border border-blue-200 rounded-lg pt-4 pb-4 shadow-lg mt-4">
+    <div className="text-[var(--resStatTextColor)] text-2xl flex w-300 text-center justify-center gap-[240px] mx-auto bg-[var(--resStatBoxColor)] rounded-lg pt-4 pb-4 shadow-lg mt-4">
       <div>
         raw
-        <div className = "text-indigo-700 font-semibold">{raw.toFixed(0)}</div>
+        <div className = "text-[var(--resStatTextColor)] font-semibold">{raw.toFixed(0)}</div>
       </div>
       <div>
         accuracy
-        <div className = "text-indigo-700 font-semibold">{accuracy.toFixed(0)}%</div>
+        <div className = "text-[var(--resStatTextColor)] font-semibold">{accuracy.toFixed(0)}%</div>
       </div>
       <div>
         mode
-        <div className = "text-indigo-700 font-semibold">{mode}s</div>
+        <div className = "text-[var(--resStatTextColor)] font-semibold">{mode}s</div>
       </div>
       <div>
         consistency
-        <div className = "text-indigo-700 font-semibold">{
+        <div className = "text-[var(--resStatTextColor)] font-semibold">{
         consistencyVal.toFixed(0)
         }%</div>
       </div>
