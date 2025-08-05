6b4f317d09953064da5df61c185134a0e6efaef5
Ans Christy <jovanacarmel@gmail.com>
2025-08-05 13:11:38 +0530

    changed result and statbar colors

--- app/components/results.tsx
+++ app/components/results.tsx
@@ -2,7 +2,7 @@ type Resultsprop = { accuracy: number; wpm: number };
 
 export default function Results({ accuracy, wpm }: Resultsprop) {
   return (
-    <div className="bg-[var(--resStatBoxColor)] p-6 mt-3 rounded-lg shadow-lg mr-10">
+    <div className="bg-[var(--resStatBoxColor)] border border-[var(--border-color)] p-6 mt-3 rounded-lg shadow-lg mr-10">
       <div className="text-[var(--resStatTextColor)] text-4xl m-auto mb-4">
         wpm
         <div className="text-5xl text-[var(--resStatTextColor)] font-bold">{wpm.toFixed(0)}</div>
--- app/components/statbar.tsx
+++ app/components/statbar.tsx
@@ -8,7 +8,7 @@ export default function Statbar({ raw, accuracy, mode,consistency }: Statbarprop
   }
   
   return (
-    <div className="text-[var(--resStatTextColor)] text-2xl flex w-300 text-center justify-center gap-[240px] mx-auto bg-[var(--resStatBoxColor)] rounded-lg pt-4 pb-4 shadow-lg mt-4">
+    <div className="text-[var(--resStatTextColor)] border border-[var(--border-color)] text-2xl flex w-300 text-center justify-center gap-[240px] mx-auto bg-[var(--resStatBoxColor)] rounded-lg pt-4 pb-4 shadow-lg mt-4">
       <div>
         raw
         <div className = "text-[var(--resStatTextColor)] font-semibold">{raw.toFixed(0)}</div>
